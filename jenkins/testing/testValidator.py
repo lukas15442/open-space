6b020fa271bc87d76091917ee0b0dace097b19b8
Lukas Köhler <lukas.koehler@h-da.de>
2018-07-23 14:26:53 +0200

    new jenkins pipeline testings

--- jenkins/testing/testValidator.py
+++ jenkins/testing/testValidator.py
@@ -4,20 +4,20 @@ import time
 import requests
 from jenkins import Jenkins
 
-URL = 'https://jenkins-jenkins.192.168.99.100.nip.io'
-USERNAME = 'restapiuser-admin'
-PASSWORD = '605624ad2448b011a50f60dac9b1e1b6'
+URL = 'https://jenkins-opensubmit.192.168.99.100.nip.io'
+USERNAME = 'developer-admin'
+PASSWORD = '69fcd7c0a6fdf5dad5b4f4e5292c8baf'
 JOB = 'Test'
-FOLDER = 'testing'
+FOLDER = '/tmp/testing'
+
+os.system('chmod -R 777 ' + FOLDER)
 
 os.environ['PYTHONHTTPSVERIFY'] = '0'
 requests.packages.urllib3.disable_warnings()
 
 server = Jenkins(URL, USERNAME, PASSWORD)
 
-job = False
-
-queue_number = server.build_job(JOB, {'folder': FOLDER})
+queue_number = server.build_job(JOB, {'folder': '/opensubmit/' + FOLDER.split('/')[2]})
 queue_item = server.get_queue_item(queue_number)
 
 while 'executable' not in queue_item:
@@ -28,7 +28,7 @@ while 'executable' not in queue_item:
 build_number = queue_item['executable']['number']
 
 build_output = server.get_build_console_output(JOB, build_number)
-while build_output.split('\n')[-2] != 'Finished: SUCCESS':
+while 'Finished:' not in build_output:
     print('Waiting for complete pipeline output . . .')
     time.sleep(2)
     build_output = server.get_build_console_output(JOB, build_number)
--- jenkins/testing/validate.py
+++ jenkins/testing/validate.py
@@ -1,17 +1,40 @@
+import os
 import time
+
+import requests
+from jenkins import Jenkins
 
 
 def validate(job):
-    print(job.working_dir)
-    print(job.timeout)
-    print(job.submission_id)
-    print(job.file_id)
-    print(job.submitter_name)
-    print(job.submitter_student_id)
-    print(job.submitter_studyprogram)
-    print(job.author_names)
-    print(job.course)
-    print(job.assignment)
-    time.sleep(60)
+    URL = 'https://jenkins-opensubmit.192.168.99.100.nip.io'
+    USERNAME = 'developer-admin'
+    PASSWORD = '69fcd7c0a6fdf5dad5b4f4e5292c8baf'
+    JOB = 'Test'
+    FOLDER = job.working_dir
 
-    job.send_pass_result("sdfsdf")
+    os.system('chmod -R 777 ' + FOLDER)
+
+    os.environ['PYTHONHTTPSVERIFY'] = '0'
+    requests.packages.urllib3.disable_warnings()
+
+    server = Jenkins(URL, USERNAME, PASSWORD)
+
+    queue_number = server.build_job(JOB, {'folder': '/opensubmit/' + FOLDER.split('/')[2]})
+    queue_item = server.get_queue_item(queue_number)
+
+    while 'executable' not in queue_item:
+        print('Waiting for queue item . . .')
+        time.sleep(2)
+        queue_item = server.get_queue_item(queue_number)
+
+    build_number = queue_item['executable']['number']
+
+    build_output = server.get_build_console_output(JOB, build_number)
+    while 'Finished:' not in build_output:
+        print('Waiting for complete pipeline output . . .')
+        time.sleep(2)
+        build_output = server.get_build_console_output(JOB, build_number)
+
+    print(build_output)
+
+    job.send_pass_result(build_output)
